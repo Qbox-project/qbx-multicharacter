06b692780e904bd0a05aacf6c9a091ccd8aa887f
JericoFX <56100605+JericoFX@users.noreply.github.com>
2022-10-11 09:18:58 -0300

    Update config.lua
    Co-authored-by: David Malchin <malchin459@gmail.com>

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped a
 Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
 Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
-Config.Debug = false -- With the change of a while true over low game event, if you try to restart the script to test some stuff it wont work because the game event only get triggered when the player join the server, With this set to TRUE if you restart the script it will trigger the event to open the NUI
+Config.Debug = false -- With this set to `true`, when the script is restarted the character selection screen will show up
 Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
 Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
     { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
