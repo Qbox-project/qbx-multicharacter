4a9772441a1612689a5a6961ca5ff8e81cd4f481
hakanesnn <106948628+hakanesnn@users.noreply.github.com>
2023-08-28 14:13:19 +0300

    fix(client): prevent error if spawnmanager is not present (#22)
    * Update main.lua
    
    protected call for servers not using spawnmanager
    
    * Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -173,16 +173,19 @@ RegisterNUICallback('removeCharacter', function(data, cb)
 end)
 
 CreateThread(function()
+	local modelHash = `mp_m_freemode_01`
 	while true do
 		Wait(0)
 		if NetworkIsSessionStarted() then
-            exports.spawnmanager:setAutoSpawn(false)
-            Wait(250)
-			TriggerEvent('qb-multicharacter:client:chooseChar')
-            while not HasModelLoaded(`mp_m_freemode_01`) do RequestModel(`mp_m_freemode_01`) Wait(0) end
-            while GetEntityModel(cache.ped) ~= `mp_m_freemode_01` do SetPlayerModel(cache.playerId, `mp_m_freemode_01`) Wait(0) end
-            SetModelAsNoLongerNeeded(`mp_m_freemode_01`)
-			break
-		end
+	            pcall(function() exports.spawnmanager:setAutoSpawn(false) end)
+	            Wait(250)
+	            TriggerEvent('qb-multicharacter:client:chooseChar')
+	            lib.requestModel(modelHash)
+	            while GetEntityModel(cache.ped) ~= modelHash do
+	                SetPlayerModel(cache.playerId, modelHash)
+	                Wait(0)
+	            end
+	            break
+	        end
 	end
 end)
